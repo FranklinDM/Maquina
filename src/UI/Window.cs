aaa5b569b020716d454ff39f1fa3bf628043ab58
FranklinDM <mrmineshafter17@gmail.com>
2019-06-21 13:54:10 +0800

    Update Window Handling: Part 1

--- src/UI/Window.cs
+++ src/UI/Window.cs
@@ -18,20 +18,13 @@ namespace Maquina.UI
             SceneManager = Global.SceneManager;
             InputManager = Global.InputManager;
             Game = Global.Game;
-            SpriteBatch = Global.SpriteBatch;
+            SpriteBatch = Global.SpriteBatch; 
             Fonts = Global.Fonts;
-            // Initialize element containers
-            Children = new Dictionary<string, GenericElement>();
-            WindowDecorations = new Dictionary<string, GenericElement>()
-            {
-                // TODO: Add other titlebar elements
-                { "background", new Image("bg") {
-                    OnUpdate = (element) =>
-                    {
-                        // TODO: continually adjust window bg size
-                    }
-                }},
-            };
+            // TODO: add handling for visibility, style, features, caption buttons
+            Visibility = Visibility.Visible;
+            Style = WindowStyle.Default;
+            Features = WindowFeatures.Default;
+            CaptionButtons = CaptionButtons.Default;
         }
 
         // Convenient aliases to global props
@@ -44,6 +37,10 @@ namespace Maquina.UI
         public string Title { get; private set; }
         public bool IsFocused { get; set; }
         public Visibility Visibility { get; set; }
+        public WindowStyle Style { get; set; }
+        public WindowFeatures Features { get; set; }
+        public WindowStates States { get; set; }
+        public CaptionButtons CaptionButtons { get; set; }
         public Vector2 Location { get; set; }
         public Vector2 Dimensions { get; set; }
         public Rectangle Bounds
@@ -65,16 +62,81 @@ namespace Maquina.UI
         }
         // Fields
         private bool IsFirstUpdateDone = false;
-
         public virtual void LoadContent()
         {
 #if HAS_CONSOLE && LOG_VERBOSE
             Console.WriteLine("Loading content from window in: {0}", Title);
 #endif
+            Children = new Dictionary<string, GenericElement>();
+
+            Texture2D BaseTexture = new Texture2D(Game.GraphicsDevice, 1, 1);
+            BaseTexture.SetData(new Color[] { Color.White });
+
+            WindowDecorations = new Dictionary<string, GenericElement>()
+            {
+                // TODO: Add other titlebar elements
+                { "background", new Image("bg") {
+                    OnUpdate = (element) =>
+                    {
+                        element.Graphic = BaseTexture;
+                        element.Tint = Style.BackgroundColor;
+                        element.DestinationRectangle = Bounds;
+                        element.LayerDepth = 0.9f;
+                        
+                        bool isMoving = (States & WindowStates.IsMoving) == WindowStates.IsMoving;
+                        if ((Bounds.Contains(InputManager.MousePosition) || isMoving) &&
+                            InputManager.MouseDown(MouseButton.Left))
+                        {
+                            // States.IsMoving = true;
+                            States |= WindowStates.IsMoving;
+                            Location += InputManager.MousePositionDifference;
+                        }
+                        else if (InputManager.MouseUp(MouseButton.Left))
+                        {
+                            // States.IsMoving = false;
+                            States &= ~WindowStates.IsMoving;
+                        }
+                    }
+                }},
+                { "border", new Image("bd") {
+                    OnUpdate = (element) =>
+                    {
+                        element.Graphic = BaseTexture;
+                        element.Tint = Style.BorderColor;
+                        Vector2 newLocation = Location - new Vector2(Style.BorderWidth);
+                        Vector2 newDimensions = Dimensions + new Vector2(Style.BorderWidth * 2);
+                        element.DestinationRectangle = new Rectangle(newLocation.ToPoint(), newDimensions.ToPoint());
+                    }
+                }},
+                { "titlebarContainer", new StackPanel("stack") {
+                    Orientation = Orientation.Horizontal,
+                    OnUpdate = (element) =>
+                    {
+                        element.Graphic = Global.Textures["cursor-default"];
+                        //element.Tint = Style.BorderColor;
+                        Vector2 newLocation = Location - new Vector2(Style.BorderWidth * 10);
+                        Vector2 newDimensions = Dimensions + new Vector2(Style.BorderWidth);
+                        element.DestinationRectangle = new Rectangle(newLocation.ToPoint(), newDimensions.ToPoint());
+                        element.Location = newLocation;
+                        element.Dimensions = newDimensions;
+                        //Console.Write(element.Location);
+                    },
+                    Children =
+                    {
+                        { "dummy", new Image("d") {
+                            //Graphic = Global.Textures["cursor-default"]
+                        }},
+                    }
+                }},
+            };
         }
 
         public virtual void Draw(GameTime gameTime)
         {
+            if (Visibility == Visibility.Hidden)
+            {
+                return;
+            }
             // Sprite batch for window decorations
             SpriteBatch.Begin(SpriteSortMode.BackToFront);
             DrawElements(gameTime, WindowDecorations);
@@ -111,12 +173,30 @@ namespace Maquina.UI
 
         public virtual void Update(GameTime gameTime)
         {
+            if (Visibility == Visibility.Hidden)
+            {
+                return;
+            }
             if (!IsFirstUpdateDone)
             {
                 IsFirstUpdateDone = true;
             }
             // TODO: Handle window movement and resize events
+            UpdateObjects(gameTime, WindowDecorations);
         }
+
+        public virtual void UpdateObjects(GameTime gameTime, IDictionary<string, GenericElement> objects)
+        {
+            UpdateObjects(gameTime, objects.Values);
+        }
+        public virtual void UpdateObjects(GameTime gameTime, IEnumerable<GenericElement> objects)
+        {
+            for (int i = 0; i < objects.Count(); i++)
+            {
+                objects.ElementAt(i).Update(gameTime);
+            }
+        }
+
 
         public virtual void Unload()
         {
